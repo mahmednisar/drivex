0ebb2710ee93d55cfba512b693a588699b99c34d
mahmednisar <16TEC2CS017@vgu.ac.in>
2020-05-23 15:42:07 +0530

    update

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Administrator\source\repos\drive\drive\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
